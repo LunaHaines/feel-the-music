1c6716f4e0474c829949e753d4f4d5ae074ef262
LunaHaines <nhaines95@gmail.com>
2021-10-14 19:03:41 -0600

    Created a Header

---
+++ src/components/toast/Header.tsx
@@ -0,0 +1,15 @@
+function Header() {
+    return (
+        <>
+            <img
+                src='../../../public/logo192.png'
+                className='rounded me-2'
+                alt=''
+            />
+            <strong className='me-auto'>Feel The Music</strong>
+            <small className='text-muted'>just now</small>
+        </>
+    )
+}
+
+export default Header;
