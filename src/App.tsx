8f54c942553944952e25ecf4da7114c474a0851d
LunaHaines <nhaines95@gmail.com>
2021-10-14 19:12:35 -0600

    added notification test

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
+import { useDispatch } from 'react-redux';
+import { showToast } from './state-slices/toast/toast-slice';
+import Header from './components/toast/Header';
 
 function App() {
+  const dispatch = useDispatch();
+
+  let payload = {
+    body: 'it works!',
+    header: Header,
+  }
+
+  let handleClick = () => {
+    dispatch(showToast(payload));
+  }
+
   return (
     <Router>
       <Switch>
         
       </Switch>
+      <Button onClick={() => handleClick()}>Show Notification</Button>
     </Router>
   );
 }
